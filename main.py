c4da4fc740cb1dbf0da652a07e4c1feb35884ff8
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-08-25 10:16:36 +0530

    Improving accuracy

--- main.py
+++ main.py
@@ -37,10 +37,6 @@ app = wx.App(False)
 (screenx,screeny) = wx.GetDisplaySize()
 (capturex,capturey) = (700,400)#captures this size frame
 
-
-vid.set(3,capturex)
-vid.set(4,capturey)
-
 kernelOpen = np.ones((5,5))#if noise are present other than yellow area
 kernelClose = np.ones((20,20)) #if noise are present in yellow area
 
@@ -79,77 +75,83 @@ while(True):
 
     #find contours
         contours,hierarchy= cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-
-   #find contour of max area(hand)
-        cnt = max(contours, key = lambda x: cv2.contourArea(x))
-
-    #approx the contour a little
-        epsilon = 0.0005*cv2.arcLength(cnt,True)
-        approx= cv2.approxPolyDP(cnt,epsilon,True)
-
-    #make convex hull around hand
-        hull = cv2.convexHull(cnt)
-
-    #define area of hull and area of hand
-        areahull = cv2.contourArea(hull)
-        areacnt = cv2.contourArea(cnt)
-
-    #find the percentage of area not covered by hand in convex hull
-        arearatio=((areahull-areacnt)/areacnt)*100
-
-    #find the defects in convex hull with respect to hand
-        hull = cv2.convexHull(approx, returnPoints=False)
-        defects = cv2.convexityDefects(approx, hull)
-
-    # l = no. of defects
-        l=0
-
-    #code for finding no. of defects due to fingers
-        for i in range(defects.shape[0]):
-            s,e,f,d = defects[i,0]
-            start = tuple(approx[s][0])
-            end = tuple(approx[e][0])
-            far = tuple(approx[f][0])
-            pt= (100,180)
-
-            # find length of all sides of triangle
-            a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
-            b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-            c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
-            s = (a+b+c)/2
-            ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
-
-            #distance between point and convex hull
-            d=(2*ar)/a
-
-            # apply cosine rule here
-            angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
-
-            # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
-            if angle <= 90 and d>25:
-                l += 1
-                cv2.circle(roi, far, 3, [255,0,0], -1)
-
-            #draw lines around hand
-            cv2.line(roi,start, end, [0,255,0], 2)
         
-        west = tuple(cnt[cnt[:, :, 0].argmin()][0]) #gives the co-ordinate of the left extreme of contour
-        east = tuple(cnt[cnt[:, :, 0].argmax()][0]) #above for east i.e right
-        north = tuple(cnt[cnt[:, :, 1].argmin()][0])
-        south = tuple(cnt[cnt[:, :, 1].argmax()][0])
-        centre_x = (west[0]+east[0])/2
-        centre_y = (north[0]+south[0])/2
-        print(areacnt)
-        cv2.circle(frame, (int(centre_x),int(centre_y)), 6, (255,0,0), -1)#plots centre of the area
-        
-        if l == 1:
-            mouse.position = (screenx-(centre_x*screenx/capturex),screeny-(centre_y*screeny/capturey))
-        elif l == 2:
-            mouse.press(Button.left)
-        elif l == 3:
-            mouse.press(Button.right)
+        if len(contours) != 0:
+        #find contour of max area(hand)
+            cnt = max(contours, key = lambda x: cv2.contourArea(x))
 
-        count_number_of_finger(l+1,frame,areacnt,arearatio)
+        #approx the contour a little
+            epsilon = 0.0005*cv2.arcLength(cnt,True)
+            approx= cv2.approxPolyDP(cnt,epsilon,True)
+
+        #make convex hull around hand
+            hull = cv2.convexHull(cnt)
+
+        #define area of hull and area of hand
+            areahull = cv2.contourArea(hull)
+            areacnt = cv2.contourArea(cnt)
+
+        #find the percentage of area not covered by hand in convex hull
+            arearatio=((areahull-areacnt)/areacnt)*100
+
+        #find the defects in convex hull with respect to hand
+            hull = cv2.convexHull(approx, returnPoints=False)
+            defects = cv2.convexityDefects(approx, hull)
+
+        # l = no. of defects
+            l=0
+
+        #code for finding no. of defects due to fingers
+            for i in range(defects.shape[0]):
+                s,e,f,d = defects[i,0]
+                start = tuple(approx[s][0])
+                end = tuple(approx[e][0])
+                far = tuple(approx[f][0])
+                pt= (100,180)
+
+                # find length of all sides of triangle
+                a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+                b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+                c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+                s = (a+b+c)/2
+                ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
+
+                #distance between point and convex hull
+                d=(2*ar)/a
+
+                # apply cosine rule here
+                angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
+
+                # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
+                if angle <= 90 and d>25:
+                    l += 1
+                    cv2.circle(roi, far, 3, [255,0,0], -1)
+
+                #draw lines around hand
+                cv2.line(roi,start, end, [0,255,0], 2)
+            
+            west = tuple(cnt[cnt[:, :, 0].argmin()][0]) #gives the co-ordinate of the left extreme of contour
+            east = tuple(cnt[cnt[:, :, 0].argmax()][0]) #above for east i.e right
+            north = tuple(cnt[cnt[:, :, 1].argmin()][0])
+            south = tuple(cnt[cnt[:, :, 1].argmax()][0])
+            centre_x = (west[0]+east[0])/2
+            centre_y = (north[0]+south[0])/2
+            
+            cv2.circle(frame, (int(centre_x),int(centre_y)), 6, (255,0,0), -1)#plots centre of the area
+            
+            l+=1
+            if arearatio<12:
+                continue
+            elif l == 1:
+                mouse.position = (screenx-(centre_x*screenx/capturex),screeny-(centre_y*screeny/capturey))
+            elif l == 2:
+                print("left button")
+                #mouse.press(Button.left)
+            elif l == 3:
+                print("right button")
+                #mouse.press(Button.right)
+
+            count_number_of_finger(l,frame,areacnt,arearatio)
         cv2.imshow('mask',mask)
         cv2.imshow('frame',frame)
     except:
